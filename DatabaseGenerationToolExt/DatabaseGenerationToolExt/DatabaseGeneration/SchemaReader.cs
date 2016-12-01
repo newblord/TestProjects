3c116030bd94b88e4e436ff41ea540b136bc2b55
Hitesh K Patel <HiteshKPatel@speedyinc.com>
2016-12-01 16:49:54 -0600

    removed StoredProcedureHelper.cs file

--- DatabaseGenerationToolExt/DatabaseGenerationToolExt/DatabaseGeneration/SchemaReader.cs
+++ DatabaseGenerationToolExt/DatabaseGenerationToolExt/DatabaseGeneration/SchemaReader.cs
@@ -340,6 +340,22 @@ namespace DatabaseGenerationToolExt.DatabaseGeneration
 			}
 		}
 
+		/// <summary>
+		/// StoredProcedure renaming ************************************************************************************************************
+		/// Use the following function to rename stored procs such as sp_CreateOrderHistory to CreateOrderHistory, my_sp_shipments to Shipments, etc.
+		/// Example:
+		/// StoredProcedureRename = (name, schema) =>
+		/// {
+		///	    if (name.StartsWith("sp_"))
+		///		    name = name.Remove(0, 3);
+		///	    return name.Replace("my_sp_", "");
+		/// };
+		/// </summary>
+		public static string StoredProcedureRename(string name, string schema)
+		{
+			return name;  // Do nothing by default
+		}
+
 		private string CleanUp(string str)
 		{
 			// Replace punctuation and symbols in variable names as these are not allowed.
